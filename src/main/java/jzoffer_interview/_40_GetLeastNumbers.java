8c6fc49aa326b174638b85b4e754643e4cff2e4b
yanwg <yanwg@channelsoft.com>
2021-06-05 12:07:32 +0800

    docs(剑指 Offer 面试): 最小的k个数
    剑指 Offer 40
    最小的k个数
    用快排最最最高效解决 TopK 问题：O(N)
    大根堆(前 K 小) / 小根堆（前 K
    大),Java中有现成的 PriorityQueue，实现起来最简单：O(NlogK)

---
+++ src/main/java/jzoffer_interview/_40_GetLeastNumbers.java
@@ -0,0 +1,30 @@
+package jzoffer_interview;
+
+/**
+ * @Author yanwg
+ * @Date 2021/6/5 11:49
+ * @Description: 最小的k个数
+ * 输入整数数组 arr ，找出其中最小的 k 个数。
+ * 例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ */
+public class _40_GetLeastNumbers {
+    /**
+     * 堆排
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int[] getLeastNumbers1(int[] arr, int k) {
+        return null;
+    }
+
+    /**
+     * 快排
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int[] getLeastNumbers2(int[] arr, int k) {
+        return null;
+    }
+}
