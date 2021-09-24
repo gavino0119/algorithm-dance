e41aa6b489a14bba08dda8d67824670c4b41999e
yanwg <yanwg@channelsoft.com>
2021-09-24 13:25:32 +0800

    53. 最大子序和 变形求最大子序列

--- src/main/java/leetcode/easy/_53_MaxSubArray.java
+++ src/main/java/leetcode/easy/_53_MaxSubArray.java
@@ -1,5 +1,7 @@
 package leetcode.easy;
 
+import java.util.Arrays;
+
 /**
  * @Author yanwg
  * @Date 2021/8/17 9:32
@@ -11,6 +13,9 @@ public class _53_MaxSubArray {
         System.out.println(maxSubArray1(arr));
 
         System.out.println(maxSubArray2(arr));
+
+        int[] nums = maxSubArray(arr);
+        System.out.println(Arrays.toString(nums));
     }
 
     /**
@@ -34,5 +39,34 @@ public class _53_MaxSubArray {
      */
     public static int maxSubArray2(int[] nums) {
         return 0;
+    }
+
+    /**
+     * 变种题，返回最大和的子序列
+     * 返回最大子序和以及起始索引和结束索引
+     * @param nums
+     * @return
+     */
+    public static int[] maxSubArray(int[] nums) {
+        {
+            int sum = 0;
+            int res = Integer.MIN_VALUE;
+            int start = 0, end = 0;
+            int finS = 0, finE = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] > 0) {
+                    end = i;
+                    nums[i] += nums[i - 1];
+                } else {
+                    start = end = i;
+                }
+                if (res < nums[i]) {
+                    finS = start;
+                    finE = end;
+                    res = nums[i];
+                }
+            }
+            return new int[]{finS, finE, res};
+        }
     }
 }
